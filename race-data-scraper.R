798f306ceac9e911758856ac0b971e7a5ace02a6
nlangholz <nathan.langholz@redbull.com>
2018-06-12 18:14:39 -0700

    update scraper

--- race-data-scraper.R
+++ race-data-scraper.R
@@ -1,49 +1,77 @@
 
 library(rvest)
 
-url <- 'https://www.rootsandrain.com/race5977/'
-#Reading the HTML code from the website
-webpage <- read_html(url)
+
+#who should have an ID?
+#athlete
+#race
+#organizer
 
 
-race_name_dirty <- html_nodes(webpage,'#h1-title')
-race_name_data <- html_text(race_name_dirty)
-race_name_data
-race_name_dirty <- html_nodes(webpage,'h1 a')
-race_name_data <- html_text(race_name_dirty)
-race_name_data
+for(i in 1:2){
+  
+  #url <- 'https://www.rootsandrain.com/race5977/'
+  url<-paste0('https://www.rootsandrain.com/race',i,'/')
 
-organizer_dirty <- html_nodes(webpage,'p a')
-organizer_data <- html_text(organizer_dirty)[3]
-organizer_data
+  #Reading the HTML code from the website
+  webpage <- read_html(url)
+  
+  #race
+  race_name_dirty <- html_nodes(webpage,'#h1-title')
+  race_name_data <- html_text(race_name_dirty)
+  race_name_data
+  
+  #race name split
+  race_name_dirty <- html_nodes(webpage,'h1 a')
+  race_name_data <- html_text(race_name_dirty)
+  race_name_data
+  
+  #race organizer
+  organizer_dirty <- html_nodes(webpage,'p a')
+  organizer_data <- html_text(organizer_dirty)[3]
+  organizer_data
+  
+  #race date
+  date_dirty <- html_nodes(webpage,'time')
+  date_data <- html_text(date_dirty)
+  date_data
+  
+  #rider names, times, results
+  stages <- NULL
+  for(i in 1:20){
+    stage_data_dirty <- html_nodes(webpage, paste0('td:nth-child(',i,')'))
+    stage_data <- html_text(stage_data_dirty)
+    stages <- cbind(stages,stage_data)
+  }
+  stages <- as.data.frame(stages)
+  
+  #race divisions
+  division_dirty <- html_nodes(webpage,'div h2')
+  division_data <- html_text(division_dirty)
+  #division_data
 
-date_dirty <- html_nodes(webpage,'time')
-date_data <- html_text(date_dirty)
-date_data
+  #table headers
+  header_dirty <- html_nodes(webpage,'th')
+  header_data <- html_text(header_dirty)[1:dim(stages)[2]]
+  #header_data
+  
+  #change stages table names
+  names(stages) <- header_data
 
-stage_children = 1:20
-
-stages <- NULL
-for(i in 1:length(stage_children)){
-  stage_data_dirty <- html_nodes(webpage, paste0('td:nth-child(',stage_children[i],')'))
-  stage_data <- html_text(stage_data_dirty)
-  stages <- cbind(stages,stage_data)
 }
 
 
-stage_data_dirty <- html_nodes(webpage, paste0('th:nth-child(',stage_children[i],')'))
-stage_data <- html_text(stage_data_dirty)
-stage_data
 
-date_dirty <- html_nodes(webpage,'th')
-date_data <- html_text(date_dirty)
-date_data
 
-table(date_data)
 
-length(date_data)/6
-date_data[1:15]
 
-date_dirty <- html_nodes(webpage,'div h2')
-date_data <- html_text(date_dirty)
-date_data
+
+#create unique ids
+install.packages("uuid",,'http://rforge.net/',type='source')
+library(uuid)
+UUIDgenerate(FALSE)
+#TRUE sets time based UID and is increasing..
+UUIDgenerate(TRUE)
+
+my_database<- src_sqlite("adverse_events", create = TRUE) # create =TRUE creates a new database
+
